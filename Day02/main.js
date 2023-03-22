d759a705ff8b0d3c46835837a18d37d51e716aad
sara <sara.atef001@gmail.com>
2023-03-22 11:55:23 +0200

    day02

---
+++ Day02/main.js
@@ -0,0 +1,103 @@
+/**
+ * .length
+ * -------------
+ * for .. loop
+ * toUpperCase
+ * toLowerCase
+ * charAt(index)
+ * indexOf
+ * lastIndexOf
+ * startsWith
+ * endsWith
+ * includes
+ * concat
+ * split
+ * trim
+ * trimStart
+ * trimEnd
+ * substring
+ */
+
+// var fname = "Hello From ITI";
+
+// var result = fname.substring(1, 3);
+// console.log(result);
+/**
+ * arr
+ * ----------
+ * length
+ * ---------------
+ * push -- add after last element on arr
+ * unshift -- add after first element
+ * pop --> remove last
+ * shift --> remove first
+ * join
+ */
+
+// var arr = ["ahmed", "ali", true];
+// // set
+// arr.reverse();
+
+// console.log(arr);
+
+// var fname = "ITO";
+
+// var result = fname.split("").reverse().join("");
+
+// console.log(result);
+
+/** sort -->  */
+// var arr = [30, 10, 3, 40, 50]; // asci code
+// arr.sort().reverse();
+
+// console.log(arr);
+
+/** machine code
+ *
+ *
+ * Binary systrm 00 100 1 00 02 \
+ *
+ * a --> asci code --> 65 --> binary 000 11 22
+ */
+/** function parameters (prams) */
+/** function statement */
+// function add(a, b) {
+//   a = a || 0;
+//   b = b || 0;
+// }
+// /** arguments */
+
+// var result = add(3, 7);
+// console.log(result);
+
+/** functions in js
+ * ----------------
+ *  = built in functions
+ *  = user defined function
+ *      -- function statement , statement declaration
+ *      -- function expression
+ *
+ *
+ * anonumous function
+ */
+
+// var add = function (a, b) {
+//   a = a || 0;
+//   console.log(a + b);
+// };
+
+// add(2, 3);
+// console.log(fname);
+// var fname = 3;
+// console.log(fname);
+
+// function sayHello() {
+//   return "dot net track";
+// }
+
+// var result = sayHello();
+// console.log(result);
+
+/** Call back function
+ * search
+ */
