1f5f1dcdc4e14fa67d4dc05b2bb2433d0e28f94e
sara53 <sara.atef001@gmail.com>
2023-03-23 11:40:36 +0200

    day03

---
+++ Day03/demo.js
@@ -0,0 +1,90 @@
+// var person = {
+//   fname: "ali",
+//   lname: "ahmed",
+//   age: 30,
+//   address: {
+//     city: "banha",
+//     street: "saad str",
+//     code: 123,
+//   },
+//   color: ["red", "green", "blue"],
+
+//   /** method */
+//   display: function () {
+//     console.log(person.fname);
+//   },
+// };
+
+// var str = "hello";
+// console.log(str.charAt("0"));
+
+/** user defined Object */
+// person.display();
+
+// var result = person.address.city;
+// var result = person.address["city"];
+
+// console.log(result);
+// console.log(person.color[2]);
+
+// console.log(person.color);
+/**
+ *
+ * set, override
+ * add
+ */
+
+// person.fname = "alaa";
+// person["fname"] = "alaa";
+// person["color"] = "red";
+
+// person.color = "red";
+
+// console.log(person);
+
+// get
+// var result = person2.age;
+// var result = person1["age"];
+// console.log(result);
+/** get  , set
+ * ----------------------
+ * dot notation
+ * subscript notation
+ */
+
+/**
+ * Date (self study)
+ */
+
+/** Math
+ * -----------
+ * PI
+ *---------------
+    == min
+    == max
+    == abs
+    == sign --> 1 , -1 , 0
+    == round --> 
+    == floor
+    == ceil
+    == sqrt
+    == random()
+ */
+
+// console.log(Math.sin((90 * Math.PI) / 180));
+// var myKey = "fname";
+// var person = {
+//   [myKey]: "ali",
+// };
+// console.log(person);
+// var phone = "phoneNumber";
+// var arr = [];
+// arr[phone] = "01222255";
+// arr["fname"] = "fname";
+// console.log(arr);
+
+/** IIFe Function (self invoked function) */
+
+// (function (name) {
+//   console.log("Hello", name);
+// })("mona");
---
+++ Day03/main.js
@@ -0,0 +1,38 @@
+/** callback function */
+
+// greet(function (trackName) {
+//   console.log("welcome ", trackName);
+// });
+
+/**
+ *  30-40 --> -10
+ * 40 - 20 -> 20
+ * 10 -10 --> 0
+ *
+ *
+ */
+
+// [30, 40, 10].sort(function (a, b) {
+//   return a - b;
+// });
+
+/**
+ * Algorithmes
+ * --------------------
+ */
+// function greet() {
+//   var trackName = "python";
+//   sayHello(trackName);
+//   return "Success";
+// }
+
+// var sayHello = function (a) {
+//   console.log(a);
+// };
+
+// var result = greet();
+// console.log(result);
+/**
+ * properties .. key : value(anyDatatype)
+ * Methods
+ */
